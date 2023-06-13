8d03b3ae05e6cb1e68cbe705b71dddb7f35892c8
profesorsitobonito <Nyaa@yamete.com>
2023-06-13 18:51:56 -0400

    Probando cosas

--- views/masterjuegos.sql
+++ views/masterjuegos.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 03-05-2023 a las 05:14:17
--- Versión del servidor: 10.4.27-MariaDB
--- Versión de PHP: 8.2.0
+-- Tiempo de generación: 14-06-2023 a las 00:51:14
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,10 +24,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `category`
+-- Estructura de tabla para la tabla `categorys`
 --
 
-CREATE TABLE `category` (
+CREATE TABLE `categorys` (
   `category_id` int(11) NOT NULL,
   `category_name` varchar(150) NOT NULL,
   `status` int(11) NOT NULL DEFAULT 1
@@ -36,10 +36,10 @@ CREATE TABLE `category` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `home`
+-- Estructura de tabla para la tabla `homes`
 --
 
-CREATE TABLE `home` (
+CREATE TABLE `homes` (
   `home_id` int(11) NOT NULL,
   `first_page_title` text NOT NULL,
   `first_page_description` text NOT NULL,
@@ -55,10 +55,10 @@ CREATE TABLE `home` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `product`
+-- Estructura de tabla para la tabla `products`
 --
 
-CREATE TABLE `product` (
+CREATE TABLE `products` (
   `product_uuid` varchar(36) NOT NULL,
   `product_name` varchar(255) NOT NULL,
   `product_price` double NOT NULL,
@@ -77,10 +77,10 @@ CREATE TABLE `product` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `product_img`
+-- Estructura de tabla para la tabla `product_imgs`
 --
 
-CREATE TABLE `product_img` (
+CREATE TABLE `product_imgs` (
   `product_img_id` int(11) NOT NULL,
   `product_base64` text NOT NULL,
   `product_uuid` varchar(255) NOT NULL
@@ -89,13 +89,15 @@ CREATE TABLE `product_img` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `user`
+-- Estructura de tabla para la tabla `users`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE `users` (
   `user_uuid` varchar(36) NOT NULL,
   `user_name` varchar(150) NOT NULL,
-  `user_password` text NOT NULL
+  `user_password` text NOT NULL,
+  `updatedAt` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -103,35 +105,35 @@ CREATE TABLE `user` (
 --
 
 --
--- Indices de la tabla `category`
+-- Indices de la tabla `categorys`
 --
-ALTER TABLE `category`
+ALTER TABLE `categorys`
   ADD PRIMARY KEY (`category_id`);
 
 --
--- Indices de la tabla `home`
+-- Indices de la tabla `homes`
 --
-ALTER TABLE `home`
+ALTER TABLE `homes`
   ADD PRIMARY KEY (`home_id`);
 
 --
--- Indices de la tabla `product`
+-- Indices de la tabla `products`
 --
-ALTER TABLE `product`
+ALTER TABLE `products`
   ADD PRIMARY KEY (`product_uuid`),
   ADD KEY `FK_694c772e-8a5a-4306-8445-05a85fd7b486` (`product_category_id`);
 
 --
--- Indices de la tabla `product_img`
+-- Indices de la tabla `product_imgs`
 --
-ALTER TABLE `product_img`
+ALTER TABLE `product_imgs`
   ADD PRIMARY KEY (`product_img_id`),
   ADD KEY `FK_c5053f55-0cb1-4a66-81af-020c5f055ca4` (`product_uuid`);
 
 --
--- Indices de la tabla `user`
+-- Indices de la tabla `users`
 --
-ALTER TABLE `user`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`user_uuid`);
 
 --
@@ -139,9 +141,9 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT de la tabla `product_img`
+-- AUTO_INCREMENT de la tabla `product_imgs`
 --
-ALTER TABLE `product_img`
+ALTER TABLE `product_imgs`
   MODIFY `product_img_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
@@ -149,16 +151,16 @@ ALTER TABLE `product_img`
 --
 
 --
--- Filtros para la tabla `product`
+-- Filtros para la tabla `products`
 --
-ALTER TABLE `product`
-  ADD CONSTRAINT `FK_694c772e-8a5a-4306-8445-05a85fd7b486` FOREIGN KEY (`product_category_id`) REFERENCES `category` (`category_id`);
+ALTER TABLE `products`
+  ADD CONSTRAINT `FK_694c772e-8a5a-4306-8445-05a85fd7b486` FOREIGN KEY (`product_category_id`) REFERENCES `categorys` (`category_id`);
 
 --
--- Filtros para la tabla `product_img`
+-- Filtros para la tabla `product_imgs`
 --
-ALTER TABLE `product_img`
-  ADD CONSTRAINT `FK_c5053f55-0cb1-4a66-81af-020c5f055ca4` FOREIGN KEY (`product_uuid`) REFERENCES `product` (`product_uuid`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `product_imgs`
+  ADD CONSTRAINT `FK_c5053f55-0cb1-4a66-81af-020c5f055ca4` FOREIGN KEY (`product_uuid`) REFERENCES `products` (`product_uuid`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
